f38f46d710ede973e22dd323bb1ecc4970e7856e
Etay Luz <etayluz@gmail.com>
2013-03-05 00:26:20 -0800

    Insert play/pause button

--- js/music.js
+++ js/music.js
@@ -15,8 +15,9 @@ var keys = [
             [113,10,0,0]        // 11 - BLACK
            ];
 
-tempo = 2150;
-
+var tempo = 2150;
+var measure = 2;
+var oldMeasure = 0;
 $(document).ready(function() {
 	/* Load the MIDI Player*/
 	MIDI.loadPlugin({
@@ -24,24 +25,39 @@ $(document).ready(function() {
 		instrument: "acoustic_grand_piano",
 		callback: function() {
 			// MIDI Player has loaded, so now allow user interaction
-			measure = 2;
-			for (var i=0;tune.length;i++)
-			{
-				if (tune[i][2] < 4* (measure - 1))
-				{
-					continue;
-				}
-				else if (tune[i][2] >= 4 * measure - 0)
-				{
-					break;
-				}
-		
-				playNote(tune[i][0],tune[i][1],tune[i][2] - (measure - 1) * 4,tune[i][3]);
-			}
+			var play = '<button type="button" onclick="play()" style="color: green; width:60px;height:28px;position:absolute;top:135px;left:175px">Play</button>';
+			var pause = '<button type="button" onclick="pause()" style="color: green; width:60px;height:28px;position:absolute;top:135px;left:245px">Pause</button>';
+			$("#button").after(play);
+			$("#button").after(pause);
 		}
+		    
 	});	
 	
 });
+
+function play()
+{
+	for (var i=0;tune.length;i++)
+	{
+		if (tune[i][2] < 4* (measure - 1))
+		{
+			continue;
+		}
+		else if (tune[i][2] >= 4 * measure - 0)
+		{
+			break;
+		}
+	
+		playNote(tune[i][0],tune[i][1],tune[i][2] - (measure - 1) * 4,tune[i][3]);
+	}	
+	
+}
+
+function pause()
+{
+	
+	
+}
 
 function debug(param)
 {
@@ -55,39 +71,96 @@ function playNote(note, velocity, delay, duration)
 	var key = note - 21;
 	var idxKey = key % 12;
 	var octave = Math.floor(key/12);
-	
+
 	var color = "green";
 	if (note < 55 && duration > 0.4)
 	{
 		color = "red";
 	}
-	
+
 	//debug("Note="+note+"<br>Key="+key+"<br>idxKey="+idxKey+"<br>octave="+octave);
 	topRecOffset = 7 + keys[idxKey][0] + 119 * octave;
 	botRecOffset = 7 + keys[idxKey][2] + 119 * octave;
 	var topRec='<div id="'+note+'topRec" style="position: absolute; top: 211px; left: '+topRecOffset+'px; background-color:'+color+';width:'+keys[idxKey][1]+'px;height:42px;border:0px solid #000"></div>';
 	var botRec='<div id="'+note+'botRec" style="position: absolute; top: 253px; left: '+botRecOffset+'px; background-color:'+color+';width:'+keys[idxKey][3]+'px;height:31px;border:0px solid #000"></div>';
-	
 
-	
 	// Turn note on (sound + visual)
 	setTimeout(function() {
 		//debug("ON " + delay + " " + note + " " + duration);
 		MIDI.setVolume(0, 127);
 		MIDI.noteOn(0, note, velocity, 0);                        
-		$("img").after(topRec);
-		$("img").after(botRec);
+		$("#keyboard").after(topRec);
+		$("#keyboard").after(botRec);
 	}, (delay)*tempo);
-	
+
 	// Turn note off (sound)
 	setTimeout(function() {
 		//debug("OFF " + delay + " " + note);
 		MIDI.noteOff(0, note, 0);
 	}, (delay+duration)*tempo);
-	
+
 	// Hide note (visualy)
 	setTimeout(function() {
 		$("#"+note+"topRec").remove();
 		$("#"+note+"botRec").remove();
 	}, (delay+duration)*tempo - tempo/20); // Subtract a little so that if the note is pressed again it is visible
 }
+
+/* --- ================ SLIDER ================== */
+
+var slider1, slider2;
+function jr() {
+    slider1 = new dhtmlxSlider("sliderBox1", 260, "dhx_skyblue");
+    slider1.setImagePath("./slider/imgs/");
+    slider1.setStep(50);
+    slider1.attachEvent("onChange", function(nv) {
+	document.getElementById("qual").value = nv;
+    });
+    //slider1.init();
+    slider2 = new dhtmlxSlider("sliderBox2", 260, "dhx_skyblue");
+    slider2.setImagePath("./slider/imgs/");
+    slider2.attachEvent("onChange", function(nv) {
+	document.getElementById("rate").value = nv;
+    });
+    slider2.setMax(50);
+    slider2.init();
+};
+
+function updateSlider(cd, val) {
+    if (isNaN(Number(val)))
+	val = 0;
+    if (cd == '1') {
+	if (val > 50 && val < 100)
+	    val = 50;
+	else if (val < 50 && val > 0)
+	    val = 0;
+	slider1.setValue(val);
+    } else {
+	if (val > 50)
+	    val = 50;
+	slider2.setValue(val);
+	document.getElementById("rate").value = val;
+    }
+};
+
+function debug(param)
+{
+      param = param + "<br>";
+      $("debug").before(param);
+}
+
+function onMouseDownSlider()
+{
+	oldMeasure = measure;
+	debug("Down");
+}
+
+function onMouseUpSlider()
+{
+	if (oldMeasure != measure)
+	{
+		measure = measure;
+	}
+	debug("Up");
+}
+
