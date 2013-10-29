69061244448811b5e23f67a2609f253cd88e095e
Etay Luz <etayluz@gmail.com>
2013-10-29 17:01:52 -0400

    Place google analytics before js files so that the event can be invoked

--- production/index.php
+++ production/index.php
@@ -1,10 +1,10 @@
 <html>
 <head>
+<?php include_once("php/analyticstracking.php") ?>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 <script src="./app.js"></script>
 </head>
 <body>
-<?php include_once("php/analyticstracking.php") ?>
 
 <!-- Loading Animation -->
 <div id="loading"  style="position:absolute;top:100px;left:500px;z-index:3;font-size:12px"><img src="./images/loading.gif"></img></div>
