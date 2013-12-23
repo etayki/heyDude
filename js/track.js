34264c2211ba59f446ef740d10c42f33afd763bb
Etay Luz <etayluz@gmail.com>
2013-12-23 00:17:20 -0500

    Refactor

--- js/track.js
+++ js/track.js
@@ -15,25 +15,7 @@ function drawMeasureTrack()
 	measureBoxTop = topOffset;
 
 	/* MEASURE TRACK */
-	// console.log("track start: " + (new Date().getTime() - startTime));
-
-	// for (number = 1; number < tune.length; number++)
-	// {			
-	// 	$("#track").append('<div id="measureBox-'+number+'" class="measureBox" style="cursor:pointer;z-index:0;border-style:solid;'+
-	// 					 'border-width:0 0 1 1;position:absolute;background-color:#FFFF99;'+
-	// 					 'left:'+measureBoxLeft+';top:0%;width:'+measureBoxWidth+';height:'+Math.floor(screenWidth*0.04)+'"></div>');
- //    	if (number != 0 &&(number%5==0 || number==1) && number < tune.length)
- //    	{
- //        	$("#measureBox-"+number).append('<b><div class="msrBoxLabel" id="msrBoxLabel-'+number+'" style="z-index:0;text-align:center;'+
- //        					   'position:absolute;left:0%;top:35%;width:100%;height:35%;background-color:clear">'+number+'</div></b>');
- //        }
-	// 	measureBoxLeft += measureBoxWidth;
-	// }
-	// console.log("track end: " + (new Date().getTime() - startTime));
-
-	//grid width and height
 	console.log("canvas start: " + (new Date().getTime() - startTime));
-
 	var canvas = $('<canvas id="canvas" style="cursor:pointer"/>').attr({width:screenWidth,height:screenWidth*0.04}).appendTo('#track');
 	var context = canvas.get(0).getContext("2d");
 	context.translate(measureBoxWidth*4, 0);
@@ -47,7 +29,6 @@ function drawMeasureTrack()
 		   					   'position:absolute;left:'+((msr+3)*measureBoxWidth+1)+';top:35%;width:'+measureBoxWidth+';height:35%;background-color:clear">'+msr+'</div></b>');
 	   	}
 	}
-
 	console.log("canvas end: " + (new Date().getTime() - startTime));
 
 	$(".msrBoxLabel").css("font-size",measureBoxLabelFontSize[$("#msrBoxLabel-5").height()]);
@@ -72,8 +53,6 @@ function drawMarkers()
 								'left:0%;top:0%;width:100%;height:100%">'+startMeasure+'</div>');
 	$("#currentMarker").append('<div id="currentMarkerLine" style="z-index:1;position:absolute;background-color:green;'+
 		                        'left:'+(measureBoxWidth*1.5-1)+';top:102%;width:'+Math.floor(measureBoxWidth*0.3)+';height:'+($("#track").height()+1)+'"></div>');
-
-	//$("#currentMarkerLabel").css("font-size",getFontSize($("#currentMarkerLabel").height()));
 
 	/* START MARKER */
 	$("#track").append('<div id="startMarker" class="marker" style="cursor:pointer;z-index:10;position:absolute;'+
