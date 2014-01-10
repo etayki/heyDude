d4f39f8b8cd44dbe578c0318419cfee6ea1bf95e
Etay Luz <etayluz@gmail.com>
2014-01-10 03:26:48 -0500

    Save work

--- js/track.js
+++ js/track.js
@@ -1,4 +1,4 @@
-				   	   // Idx: 0 1 2 3 4 5 6 7 8 9 0 1 2 3 14 15 16 17 18 19 20 21 22 23 24
+c				   	   // Idx: 0 1 2 3 4 5 6 7 8 9 0 1 2 3 14 15 16 17 18 19 20 21 22 23 24
 var measureBoxLabelFontSize = [0,1,1,1,2,2,3,4,4,6,7,8,8,8,10,11,12,12,14,15,15,16,16,18,19];
 var markerOffset = activeMarker = null;
 
@@ -49,7 +49,7 @@ function drawMeasureTrack()
 function drawMarkers()
 {
 	/* CURRENT MARKER */
-	$("#trackHeader").append('<div id="currentMarker" class="marker" style="cursor:pointer;z-index:10;background-color:clear;position:absolute;'+
+	$("#trackHeader").append('<div id="currentMarker" class="marker" style="cursor:pointer;z-index:5;background-color:clear;position:absolute;'+
 					 'left:'+(measureBoxWidth*2.5)+';top:0%;width:'+(measureBoxWidth*3)+';height:100%"><div>');
 	$("#currentMarker").append('<div id="currentMarkerLabel" style="z-index:1;position:absolute;background-color:green;text-align:center;'+
 								'left:0%;top:0%;width:100%;height:100%;box-shadow:1px 1px 1px #555555;text-shadow:0px 1px 0px rgba(220,220,220,0.3);">'+startMeasure+'</div>');
@@ -57,7 +57,7 @@ function drawMarkers()
 		                        'left:'+(measureBoxWidth*1.5-1)+';top:102%;width:'+Math.floor(measureBoxWidth*0.3)+';height:'+($("#track").height()+1)+'"></div>');
 
 	/* START MARKER */
-	$("#track").append('<div id="startMarker" class="marker" style="cursor:pointer;z-index:10;position:absolute;'+
+	$("#track").append('<div id="startMarker" class="marker" style="cursor:pointer;z-index:5;position:absolute;'+
 					   'left:0%;top:0%;width:'+measureBoxWidth*4+';height:100%"><div>');
 	$("#startMarker").append('<img id="startMarkerImg" src="./images/startMarker.png" style="z-index:1;position:absolute;'+
 							 'left:'+(measureBoxWidth*2)+';top:0%;width:'+(measureBoxWidth*2)+';height:100%">');
@@ -66,7 +66,7 @@ function drawMarkers()
 
 
 	/* END MARKER */
-	$("#track").append('<div id="endMarker" class="marker" style="cursor:pointer;z-index:10;position:absolute;'+
+	$("#track").append('<div id="endMarker" class="marker" style="cursor:pointer;z-index:5;position:absolute;'+
 					   'right:0%;top:0%;width:'+measureBoxWidth*4+';height:100%"><div>');
 	$("#endMarker").append('<img id="endMarkerImg" src="./images/endMarker.png" style="z-index:1;position:absolute;'+
 							 'right:'+(measureBoxWidth*2-1)+';top:0%;width:'+(measureBoxWidth*2)+';height:100%">');
